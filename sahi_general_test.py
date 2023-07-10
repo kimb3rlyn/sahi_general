1ef10f3f3ad6dd3f2e3ecfe7b0496b6b0558257b
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2023-07-10 17:34:25 +0800

    test script working with latest changes

--- sahi_general_test.py
+++ sahi_general_test.py
@@ -18,12 +18,10 @@ Path(output_folder).mkdir(parents=True, exist_ok=True)
 
 
 yolov7 = YOLOv7(
-    weights = './yolov7-e6-visdrone-blackout-save-state.pt',
-    cfg = './yolov7-e6-visdrone.yaml',
-    # weights='yolov7_state.pt',
-    # cfg='./yolov7.yaml',
+    weights=files('yolov7').joinpath('weights/yolov7_state.pt'),
+    cfg=files('yolov7').joinpath('cfg/deploy/yolov7.yaml'),
     bgr=True,
-    gpu_device=0,
+    device='cuda',
     model_image_size=640,
     max_batch_size=16,
     half=True,
@@ -33,35 +31,31 @@ yolov7 = YOLOv7(
     cudnn_benchmark=False,
 )
 
+classes = ['person', 'car']
 
 '''
-    SAHI library needs to be installed 
+    SAHI library needs to be installed
     Model needs to have classname_to_idx function and get_detections_dict function
     classname_to_idx : int
-        class index of the classname given 
+        class index of the classname given
     get_detections_dict : List[dict]
         list of detections for each frame with keys: label, confidence, t, l, b, r, w, h
 '''
 sahi_general = SahiGeneral(model=yolov7)
 
 img = cv2.imread(str(imgpath))
-bs = 64
+bs = 8
 imgs = [img for _ in range(bs)]
 
 dur = 0
 
 torch.cuda.synchronize()
 tic = perf_counter()
-'''
-    If one ndarray given, this returns a list (boxes in one image) of tuple (box_infos, score, predicted_class),
-    else if a list of ndarray given, this return a list (batch) containing the former as the elements.
-'''
-dets = sahi_general.detect(imgs)
+dets = sahi_general.detect(imgs, classes)
 
 torch.cuda.synchronize()
 toc = perf_counter()
 dur += toc - tic
-
 
 print(f'Average time taken: {(dur*1000):0.2f}ms')
 
@@ -69,10 +63,13 @@ draw_frame = img.copy()
 
 for chip in dets:
     for det in chip:
-        bb, score, class_ = det
-        l, t, r, b = bb
+        l = det['l']
+        t = det['t']
+        r = det['r']
+        b = det['b']
+        classname = det['label']
         cv2.rectangle(draw_frame, (l, t), (r, b), (255, 255, 0), 1)
-        cv2.putText(draw_frame, class_, (l, t-8), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0))
+        cv2.putText(draw_frame, classname, (l, t-8), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0))
 
 output_path = Path(output_folder) / 'test_out.jpg'
 cv2.imwrite(str(output_path), draw_frame)
