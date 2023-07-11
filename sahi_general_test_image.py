2da765ea24ed2e974ba0eb453f81e6848d108a5a
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2023-07-11 18:08:33 +0800

    sahi now supports batching

--- sahi_general_test_image.py
+++ sahi_general_test_image.py
@@ -13,8 +13,8 @@ imgpath = Path('test.jpg')
 if not imgpath.is_file():
     raise AssertionError(f'{str(imgpath)} not found')
 
-output_folder = 'inference'
-Path(output_folder).mkdir(parents=True, exist_ok=True)
+output_folder = Path('inference')
+output_folder.mkdir(parents=True, exist_ok=True)
 
 
 yolov7 = YOLOv7(
@@ -47,29 +47,25 @@ img = cv2.imread(str(imgpath))
 bs = 8
 imgs = [img for _ in range(bs)]
 
-dur = 0
-
 torch.cuda.synchronize()
 tic = perf_counter()
-dets = sahi_general.detect(imgs, classes)
+detections = sahi_general.detect(imgs, classes)
 
 torch.cuda.synchronize()
-toc = perf_counter()
-dur += toc - tic
+dur = perf_counter() - tic
 
-print(f'Average time taken: {(dur*1000):0.2f}ms')
+print(f'Time taken: {(dur*1000):0.2f}ms')
 
 draw_frame = img.copy()
 
-for chip in dets:
-    for det in chip:
-        l = det['l']
-        t = det['t']
-        r = det['r']
-        b = det['b']
-        classname = det['label']
-        cv2.rectangle(draw_frame, (l, t), (r, b), (255, 255, 0), 1)
-        cv2.putText(draw_frame, classname, (l, t-8), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0))
+for det in detections[0]:
+    l = det['l']
+    t = det['t']
+    r = det['r']
+    b = det['b']
+    classname = det['label']
+    cv2.rectangle(draw_frame, (l, t), (r, b), (255, 255, 0), 1)
+    cv2.putText(draw_frame, classname, (l, t-8), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0))
 
-output_path = Path(output_folder) / 'test_out.jpg'
+output_path = output_folder / 'test_out.jpg'
 cv2.imwrite(str(output_path), draw_frame)
